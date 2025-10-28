b434bb54413e36731ea1a56b83e749517bb17b20
apiwatniyom-spec <apiwatniyom@gmail.com>
2025-10-28 12:15:34 +0700

    commit2

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
     echo"<h1>My name is Briant</h1>";
     echo"<h2>ID : 11223344 </h2>";
+
+    echo"<h3>Class : Web Programming 2 </h3>";
+    echo"<h4>Major : Information Technology </h4>";
 ?>
